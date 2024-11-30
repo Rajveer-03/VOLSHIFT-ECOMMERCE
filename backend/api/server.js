9919693d78fb431a3d62a0108d8b674773121425
Rajveer-03 <veermarwaha03@gmail.com>
2024-11-30 15:39:30 +0530

    changes made

--- backend/api/server.js
+++ backend/api/server.js
@@ -47,7 +47,7 @@ app.post('/api/stripe-checkout', async (req, res) => {
     res.status(200).json({ url: session.url });
   } catch (error) {
     console.error('Stripe Checkout error:', error);
-    res.status(500).json({ error: 'Failed to create checkout session' });
+    res.status(500).json({ error: error });
   }
 });
 
